5ffaa352055c823a14219f72dbaeeb244c990870
Nataliia Globa <Nataliia_Globa@epam.com>
2019-03-20 14:09:43 +0100

    changes

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -6,8 +6,27 @@ package com.epam.training;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        hello("world");
+        hello("Nataliia");
+        hello("user");
+
+        double l = 5;
+        double m =12;
+        System.out.println("Square of "+ l +" = "+ area(l));
+        System.out.println("Square of with sides " + l+ " and "+ m + " = " +area(l,m));
+    }
+
+    public static void hello(String somebody) {
+
+        System.out.println( "Hello " + somebody + " !" );
+    }
+
+    public static double area(double len) {
+        return len*len;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
     }
 }
